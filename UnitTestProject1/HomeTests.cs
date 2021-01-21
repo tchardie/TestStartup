fb59d96e7fb102ef946572bea8b2fb0c2c44d93e
tchardie <tchardie@users.noreply.github.com>
2021-01-21 13:13:41 -0500

    initial

---
+++ UnitTestProject1/HomeTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class HomeTests
+    {
+        HomeController controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            controller = new HomeController();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            controller.Dispose();
+            controller = null;
+        }
+
+        [TestMethod]
+        public void TestIndexGet()
+        {
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow(1)]
+        public void TestAboutWithDifferentInputs(int? id)
+        {
+            var result = controller.About(id);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void TestAboutWithNullId()
+        {
+            int? id = null;
+            var result = controller.About(id);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void TestAboutWithIdAndGetName()
+        {
+            var result = controller.About(1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(JsonResult));
+
+            var data = ((JsonResult)result).Data;
+
+            Assert.IsInstanceOfType(data, typeof(ModelA));
+
+            var name = ((ModelA) data).Name;
+            Assert.AreEqual("TestName", name);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int? id)
+        {
+            ViewBag.Message = "Your application description page.";
+
+            if(id.HasValue)
+            {
+                return Json(new ModelA{ Id = id.Value, Name = "TestName" });
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
