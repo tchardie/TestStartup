f4650654bbf925949234bea991635ceac4fd8405
tchardie <tchardie@users.noreply.github.com>
2021-01-26 09:54:31 -0500

    added sample setup

--- UiTests1/Class1.cs
+++ UiTests1/Class1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace UiTests1
 {
@@ -10,7 +13,28 @@ namespace UiTests1
     {
         public Class1()
         {
-            
+            var driverService = FirefoxDriverService.CreateDefaultService(@"C:\Tools\geckodriver", "geckodriver.exe");
+
+            using (var driver = new FirefoxDriver(driverService))
+            {
+                driver.Url = "https://www.google.com";
+
+                Thread.Sleep(2000);
+
+                driver.Close();
+            }
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.False(false);
         }
     }
 }
